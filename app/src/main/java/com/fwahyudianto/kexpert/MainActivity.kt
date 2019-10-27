b36bd776ccdc430eeefba9e882ffcf2f07332bb2
fwahyudianto06 <fwahyudi06@gmail.com>
2019-10-27 11:52:58 +0700

    Initial Project

---
+++ app/src/main/java/com/fwahyudianto/kexpert/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fwahyudianto.kexpert
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
